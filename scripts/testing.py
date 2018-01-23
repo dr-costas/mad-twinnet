345473e5f56286b351d9e06833bc245f30e7b3f4
drossos <kostas.drosos@icloud.com>
2018-01-23 14:29:33 +0200

    Scripts ready

--- scripts/testing.py
+++ scripts/testing.py
@@ -4,14 +4,142 @@
 """Testing process module.
 """
 
-# imports
+import pickle
+import time
 
-__author__ = 'Konstantinos Drossos'
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+from helpers.data_feeder import data_feeder_testing, data_process_results_testing
+from helpers.settings import debug, hyper_parameters, output_states_path, \
+    testing_output_string_per_example, metrics_paths, testing_output_string_all
+from modules import RNNEnc, RNNDec, FNNMasker, FNNDenoiser
+
+__author__ = ['Konstantinos Drossos -- TUT', 'Stylianos Mimilakis -- Fraunhofer IDMT']
 __docformat__ = 'reStructuredText'
+__all__ = ['testing_process']
+
+
+def testing_process():
+    """The testing process.
+    """
+
+    print('\n-- Starting testing process. Debug mode: {}.'.format(debug))
+    print('-- Setting up modules... ', end='')
+
+    # Masker modules
+    rnn_enc = RNNEnc(hyper_parameters['reduced_dim'], hyper_parameters['context_length'], debug)
+    rnn_dec = RNNDec(hyper_parameters['rnn_enc_output_dim'], debug)
+    fnn = FNNMasker(
+        hyper_parameters['rnn_enc_output_dim'],
+        hyper_parameters['original_input_dim'],
+        hyper_parameters['context_length']
+    )
+
+    # Denoiser modules
+    denoiser = FNNDenoiser(hyper_parameters['original_input_dim'])
+
+    rnn_enc.load_state_dict(torch.load(output_states_path['rnn_enc']))
+    rnn_dec.load_state_dict(torch.load(output_states_path['rnn_dec']))
+    fnn.load_state_dict(torch.load(output_states_path['fnn']))
+    denoiser.load_state_dict(torch.load(output_states_path['denoiser']))
+
+    if not debug and torch.has_cudnn:
+        rnn_enc = rnn_enc.cuda()
+        rnn_dec = rnn_dec.cuda()
+        fnn = fnn.cuda()
+        denoiser = denoiser.cuda()
+
+    print('done.')
+
+    testing_it = data_feeder_testing(
+        window_size=hyper_parameters['window_size'], fft_size=hyper_parameters['fft_size'],
+        hop_size=hyper_parameters['hop_size'], seq_length=hyper_parameters['seq_length'],
+        context_length=hyper_parameters['context_length'], batch_size=1,
+        debug=debug
+    )
+
+    print('-- Testing starts\n')
+
+    sdr = []
+    sir = []
+    total_time = 0
+
+    for index, data in enumerate(testing_it()):
+
+        s_time = time.time()
+
+        mix, mix_magnitude, mix_phase, voice_true, bg_true = data
+
+        voice_predicted = np.zeros(
+            (
+                mix_magnitude.shape[0],
+                hyper_parameters['seq_length'] - hyper_parameters['context_length'] * 2,
+                hyper_parameters['window_size']
+            ),
+            dtype=np.float32
+        )
+
+        for batch in range(int(mix_magnitude.shape[0] / hyper_parameters['batch_size'])):
+            b_start = batch * hyper_parameters['batch_size']
+            b_end = (batch + 1) * hyper_parameters['batch_size']
+
+            v_in = Variable(torch.from_numpy(mix_magnitude[b_start:b_end, :, :]))
+
+            if not debug and torch.has_cudnn:
+                v_in = v_in.cuda()
+
+            tmp_voice_predicted = rnn_enc(v_in)
+            tmp_voice_predicted = rnn_dec(tmp_voice_predicted)
+            tmp_voice_predicted = fnn(tmp_voice_predicted, v_in)
+            tmp_voice_predicted = denoiser(tmp_voice_predicted)
+
+            voice_predicted[b_start:b_end, :, :] = tmp_voice_predicted.data.cpu().numpy()
+
+        tmp_sdr, tmp_sir = data_process_results_testing(
+            index=index, voice_true=voice_true, bg_true=bg_true,
+            voice_predicted=voice_predicted,
+            window_size=hyper_parameters['window_size'], mix=mix, mix_magnitude=mix_magnitude,
+            mix_phase=mix_phase, hop=hyper_parameters['hop_size'],
+            context_length=hyper_parameters['context_length']
+        )
+
+        e_time = time.time()
+
+        print(testing_output_string_per_example.format(
+            e=index,
+            sdr=[i for i in tmp_sdr[0] if not np.isnan(i)],
+            sir=[i for i in tmp_sir[0] if not np.isnan(i)],
+            t=e_time - s_time
+        ))
+
+        total_time += e_time - s_time
+
+        sdr.append(tmp_sdr)
+        sir.append(tmp_sir)
+
+    print('\n-- Testing finished\n')
+    print(testing_output_string_all.format(
+        sdr=np.median([ii for i in sdr for ii in i[0] if not np.isnan(ii)]),
+        sir=np.median([ii for i in sir for ii in i[0] if not np.isnan(ii)]),
+        t=total_time
+    ))
+
+    print('\n-- Saving results... ', end='')
+
+    with open(metrics_paths['sdr'], 'wb') as f:
+        pickle.dump(sdr, f, protocol=2)
+
+    with open(metrics_paths['sir'], 'wb') as f:
+        pickle.dump(sir, f, protocol=2)
+
+    print('done!')
+    print('That\'s all folks!')
 
 
 def main():
-    pass
+    testing_process()
 
 
 if __name__ == '__main__':
--- scripts/training.py
+++ scripts/training.py
@@ -4,7 +4,6 @@
 """Training process module.
 """
 
-import os
 import time
 
 import torch
@@ -12,6 +11,8 @@ from torch import optim
 from torch.autograd import Variable
 
 from helpers.data_feeder import data_feeder_training
+from helpers.settings import debug, hyper_parameters, training_constants, \
+    training_output_string, output_states_path
 from modules import RNNEnc, RNNDec, FNNMasker, FNNDenoiser, AffineTransform
 from objectives import kullback_leibler as kl, l2_loss, sparsity_penalty, l2_reg
 
@@ -24,41 +25,28 @@ def training_process():
     """The training process.
     """
 
-    # Training constants
-    debug = False
-    print('\n-- Starting. Debug mode: {}'.format(debug))
-
-    epochs = 2 if debug else 100
-    batch_size = 16
-    files_per_pass = 4
-
-    # Hyper-parameters
-    window_size = 2049
-    fft_size = 4096
-    hop_size = 384
-    seq_length = 60
-    context_length = 10
-    reduced_dim = 744
-    original_input_dim = 2049
-    rnn_enc_output_dim = 2 * reduced_dim
-    learning_rate = 1e-4
-    max_grad_norm = .5
-    lambda_l_twin = .5
-    lambda_1 = 1e-2
-    lambda_2 = 1e-4
-
+    print('\n-- Starting training process. Debug mode: {}'.format(debug))
+    print('-- Setting up modules... ', end='')
     # Masker modules
-    rnn_enc = RNNEnc(reduced_dim, context_length, debug)
-    rnn_dec = RNNDec(rnn_enc_output_dim, debug)
-    fnn = FNNMasker(rnn_enc_output_dim, original_input_dim, context_length)
+    rnn_enc = RNNEnc(hyper_parameters['reduced_dim'], hyper_parameters['context_length'], debug)
+    rnn_dec = RNNDec(hyper_parameters['rnn_enc_output_dim'], debug)
+    fnn = FNNMasker(
+        hyper_parameters['rnn_enc_output_dim'],
+        hyper_parameters['original_input_dim'],
+        hyper_parameters['context_length']
+    )
 
     # Denoiser modules
-    denoiser = FNNDenoiser(original_input_dim)
+    denoiser = FNNDenoiser(hyper_parameters['original_input_dim'])
 
     # TwinNet regularization modules
-    twin_net_rnn_dec = RNNDec(rnn_enc_output_dim, debug)
-    twin_net_fnn_masker = FNNMasker(rnn_enc_output_dim, original_input_dim, context_length)
-    affine_transform = AffineTransform(rnn_enc_output_dim)
+    twin_net_rnn_dec = RNNDec(hyper_parameters['rnn_enc_output_dim'], debug)
+    twin_net_fnn_masker = FNNMasker(
+        hyper_parameters['rnn_enc_output_dim'],
+        hyper_parameters['original_input_dim'],
+        hyper_parameters['context_length']
+    )
+    affine_transform = AffineTransform(hyper_parameters['rnn_enc_output_dim'])
 
     if not debug and torch.has_cudnn:
         rnn_enc = rnn_enc.cuda()
@@ -69,7 +57,8 @@ def training_process():
         twin_net_fnn_masker = twin_net_fnn_masker.cuda()
         affine_transform = affine_transform.cuda()
 
-    print('-- Modules set up')
+    print('done.')
+    print('-- Setting up optimizes and losses... ', end='')
 
     # Objectives and penalties
     loss_masker = kl
@@ -88,27 +77,27 @@ def training_process():
         list(twin_net_rnn_dec.parameters()) +
         list(twin_net_fnn_masker.parameters()) +
         list(affine_transform.parameters()),
-        lr=learning_rate
+        lr=hyper_parameters['hyper_parameters']
     )
 
-    print('-- Optimizer set up')
+    print('done.')
 
     # Initializing data feeder
     epoch_it = data_feeder_training(
-        window_size=window_size,
-        fft_size=fft_size,
-        hop_size=hop_size,
-        seq_length=seq_length,
-        context_length=context_length,
-        batch_size=batch_size,
-        files_per_pass=files_per_pass,
+        window_size=hyper_parameters['window_size'],
+        fft_size=hyper_parameters['fft_size'],
+        hop_size=hyper_parameters['hop_size'],
+        seq_length=hyper_parameters['seq_length'],
+        context_length=hyper_parameters['context_length'],
+        batch_size=training_constants['batch_size'],
+        files_per_pass=training_constants['files_per_pass'],
         debug=debug
     )
 
     print('-- Training starts\n')
 
     # Training loop starts
-    for epoch in range(epochs):
+    for epoch in range(training_constants['epochs']):
         epoch_l_m = []
         epoch_l_d = []
         epoch_l_tw = []
@@ -149,9 +138,9 @@ def training_process():
             l_twin = reg_twin(affine_output, h_dec_twin)
 
             # Make MaD TwinNet objective
-            loss = l_m + l_d + l_tw + (lambda_l_twin * l_twin) + \
-                   (lambda_1 * reg_fnn_masker(fnn.linear_layer.weight)) + \
-                   (lambda_2 * reg_fnn_dec(denoiser.fnn_dec.weight))
+            loss = l_m + l_d + l_tw + (hyper_parameters['lambda_l_twin '] * l_twin) + \
+                   (hyper_parameters['lambda_1'] * reg_fnn_masker(fnn.linear_layer.weight)) + \
+                   (hyper_parameters['lambda_2'] * reg_fnn_dec(denoiser.fnn_dec.weight))
 
             # Backward pass
             loss.backward()
@@ -165,7 +154,7 @@ def training_process():
                 list(twin_net_rnn_dec.parameters()) +
                 list(twin_net_fnn_masker.parameters()) +
                 list(affine_transform.parameters()),
-                max_norm=max_grad_norm, norm_type=2
+                max_norm=hyper_parameters['max_grad_norm'], norm_type=2
             )
 
             # Optimize
@@ -177,27 +166,26 @@ def training_process():
             epoch_l_tw.append(l_tw.data[0])
             epoch_l_twin.append(l_twin.data[0])
 
-        time_end = time.time() - time_start
+        time_end = time.time()
 
         # Tell us what happened
-        print(
-            'Epoch: {ep:3d} Losses: -- Masker: {l_m:.4f} | Denoiser: {l_d:.4f} | '
-            'Twin: {l_tw:.4f} | Twin reg.: {l_twin:.4f} | Time: {t:.2f} sec(s)'.format(
-                ep=epoch,
-                l_m=torch.mean(torch.FloatTensor(epoch_l_m)),
-                l_d=torch.mean(torch.FloatTensor(epoch_l_d)),
-                l_tw=torch.mean(torch.FloatTensor(epoch_l_tw)),
-                l_twin=torch.mean(torch.FloatTensor(epoch_l_tw)),
-                t=time_end
-            ))
+        print(training_output_string.format(
+            ep=epoch,
+            l_m=torch.mean(torch.FloatTensor(epoch_l_m)),
+            l_d=torch.mean(torch.FloatTensor(epoch_l_d)),
+            l_tw=torch.mean(torch.FloatTensor(epoch_l_tw)),
+            l_twin=torch.mean(torch.FloatTensor(epoch_l_tw)),
+            t=time_end - time_start
+        ))
 
     # Kindly end and save the model
-    print('\n-- Training done. Saving model')
-    torch.save(rnn_enc.state_dict(), os.path.join('states', 'rnn_enc.pt'))
-    torch.save(rnn_dec.state_dict(), os.path.join('states', 'rnn_dec.pt'))
-    torch.save(fnn.state_dict(), os.path.join('states', 'fnn.pt'))
-    torch.save(denoiser.state_dict(), os.path.join('states', 'denoiser.pt'))
-    print('-- Model saved.')
+    print('\n-- Training done.')
+    print('-- Saving model.. ', end='')
+    torch.save(rnn_enc.state_dict(), output_states_path['rnn_enc'])
+    torch.save(rnn_dec.state_dict(), output_states_path['rnn_dec'])
+    torch.save(fnn.state_dict(), output_states_path['fnn'])
+    torch.save(denoiser.state_dict(), output_states_path['denoiser'])
+    print('done.')
     print('-- That\'s all folks!')
 
 
