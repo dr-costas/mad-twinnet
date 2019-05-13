3829119f99201463268623e11a7a13173131ee60
drossos <kostas.drosos@icloud.com>
2019-05-13 14:25:08 +0300

    Updated code for RNN decoder, according to PyTorch 1.1.0.

--- modules/rnn_dec.py
+++ modules/rnn_dec.py
@@ -5,9 +5,9 @@
 """
 
 import torch
-from torch.autograd import Variable
-from torch.nn import Module, GRUCell
-from torch.nn.init import xavier_normal_, orthogonal_
+from torch.nn import Module, GRU
+from torch.nn.init import xavier_normal_, \
+    orthogonal_, constant_
 
 __author__ = ['Konstantinos Drossos -- TUT', 'Stylianos Mimilakis -- Fraunhofer IDMT']
 __docformat__ = 'reStructuredText'
@@ -15,21 +15,21 @@ __all__ = ['RNNDec']
 
 
 class RNNDec(Module):
-    def __init__(self, input_dim, debug):
+    def __init__(self, input_dim):
         """The RNN dec of the Masker.
 
         :param input_dim: The input dimensionality.
         :type input_dim: int
-        :param debug: Flag to indicate debug
-        :type debug: bool
         """
         super(RNNDec, self).__init__()
 
         self._input_dim = input_dim
-        self.gru_dec = GRUCell(self._input_dim, self._input_dim)
-
-        self._debug = debug
-        self._device = 'cuda' if not self._debug and torch.cuda.is_available() else 'cpu'
+        self.gru_dec = GRU(
+            input_size=self._input_dim,
+            hidden_size=self._input_dim,
+            num_layers=1, bias=True,
+            batch_first=True, bidirectional=False
+        )
 
         self.initialize_decoder()
 
@@ -37,29 +37,20 @@ class RNNDec(Module):
         """Manual weight/bias initialization.
         """
 
-        xavier_normal_(self.gru_dec.weight_ih)
-        orthogonal_(self.gru_dec.weight_hh)
+        xavier_normal_(self.gru_dec.weight_ih_l0)
+        orthogonal_(self.gru_dec.weight_hh_l0)
 
-        self.gru_dec.bias_ih.data.zero_()
-        self.gru_dec.bias_hh.data.zero_()
+        constant_(self.gru_dec.bias_ih_l0, 0)
+        constant_(self.gru_dec.bias_hh_l0, 0)
 
     def forward(self, h_enc):
         """The forward pass.
 
         :param h_enc: The output of the RNN encoder.
-        :type h_enc: torch.autograd.variable.Variable
+        :type h_enc: torch.Tensor
         :return: The output of the RNN dec (h_j_dec)
-        :rtype: torch.autograd.variable.Variable
+        :rtype: torch.Tensor
         """
-        batch_size = h_enc.size()[0]
-        seq_length = h_enc.size()[1]
-        h_t_dec = torch.zeros(batch_size, self._input_dim).to(self._device)
-        h_j_dec = torch.zeros(batch_size, seq_length, self._input_dim).to(self._device)
-
-        for ts in range(seq_length):
-            h_t_dec = self.gru_dec(h_enc[:, ts, :], h_t_dec)
-            h_j_dec[:, ts, :] = h_t_dec
-
-        return h_j_dec
+        return self.gru_dec(h_enc)[0]
 
 # EOF
