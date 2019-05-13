584d96c39bc003326ff23a19fbaeae004585ffe9
drossos <kostas.drosos@icloud.com>
2019-05-13 14:39:27 +0300

    Updated code and comments according to PyTorch 1.1.0.

--- modules/affine_transform.py
+++ modules/affine_transform.py
@@ -5,7 +5,7 @@
 """
 
 from torch.nn import Module, Linear
-from torch.nn.init import xavier_normal_
+from torch.nn.init import xavier_normal_, constant_
 
 __author__ = ['Konstantinos Drossos -- TUT', 'Stylianos Mimilakis -- Fraunhofer IDMT']
 __docformat__ = 'reStructuredText'
@@ -16,10 +16,9 @@ class AffineTransform(Module):
     def __init__(self, input_dim):
         """The affine transform for the TwinNet regularization.
 
-        :param input_dim: The input dimensionality
+        :param input_dim: The input dimensionality.
         :type input_dim: int
         """
-
         super(AffineTransform, self).__init__()
 
         self._input_dim = input_dim
@@ -31,15 +30,15 @@ class AffineTransform(Module):
         """Manual weight/bias initialization.
         """
         xavier_normal_(self.linear_layer.weight)
-        self.linear_layer.bias.data.zero_()
+        constant_(self.linear_layer.bias, 0)
 
     def forward(self, h_j_dec):
         """Forward pass.
 
-        :param h_j_dec: The output from the RNN decoder
-        :type h_j_dec: torch.autograd.variable.Variable
-        :return: The output of the affine transform
-        :rtype: torch.autograd.variable.Variable
+        :param h_j_dec: The output from the RNN decoder.
+        :type h_j_dec: torch.Tensor
+        :return: The output of the affine transform.
+        :rtype: torch.Tensor
         """
         return self.linear_layer(h_j_dec)
 
