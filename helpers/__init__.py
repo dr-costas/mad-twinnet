0135612cd32ba7b7d8fa36474623994b155ee13b
drossos <kostas.drosos@icloud.com>
2019-05-17 19:23:18 +0300

    Added submodules as imports.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = ['Konstantinos Drossos -- TUT', 'Stylianos Mimilakis -- Fraunhofer IDMT']
+from helpers import audio_io
+from helpers import data_feeder
+from helpers import printing
+from helpers import signal_transforms
+
+__author__ = 'Konstantinos Drossos -- TAU', 'Stylianos Mimilakis -- Fraunhofer IDMT'
 __docformat__ = 'reStructuredText'
+__all__ = [
+    'audio_io', 'data_feeder',
+    'printing', 'signal_transforms'
+]
 
 # EOF
