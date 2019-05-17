31cac8872c09629bb099d25d07ee6fbc86282a64
drossos <kostas.drosos@icloud.com>
2019-05-17 17:31:09 +0300

    Refactored the init of the modules, to clearly indicate the private and public members.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from modules.affine_transform import AffineTransform
-from modules.fnn import FNNMasker
-from modules.fnn_denoiser import FNNDenoiser
-from modules.rnn_dec import RNNDec
-from modules.rnn_enc import RNNEnc
-from modules.twin_rnn_dec import TwinRNNDec
+from modules.madtwinnet import MaDTwinNet
+from modules.mad import MaD
 
 __author__ = ['Konstantinos Drossos -- TUT', 'Stylianos Mimilakis -- Fraunhofer IDMT']
 __docformat__ = 'reStructuredText'
-__all__ = ['RNNEnc', 'RNNDec', 'FNNMasker', 'FNNDenoiser', 'TwinRNNDec', 'AffineTransform']
+__all__ = ['MaDTwinNet', 'MaD']
 
 # EOF
